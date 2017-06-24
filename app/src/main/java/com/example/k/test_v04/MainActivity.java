1656a738f6ff068db2353db9451616fea070fb92
tromso <ktromso@gmail.com>
2017-06-24 16:50:55 -0400

    vo04

--- app/src/main/java/com/example/k/test_v04/MainActivity.java
+++ app/src/main/java/com/example/k/test_v04/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     EditText text1;
     String s1 = "llll";
+    String s2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
